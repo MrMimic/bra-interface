7f5c1903d95de6c57db3b52922a8170c75ca9664
Emeric <emeric.dynomant@jellysmack.com>
2022-03-18 19:32:24 +0100

    :sparkles: historic and about are working

--- run.py
+++ run.py
@@ -1,15 +1,16 @@
 """
 """
-from datetime import datetime
 import os
+from datetime import datetime
 from typing import Dict
 from urllib import request
-from bra_interface.connection import BraDatabase
-from bra_interface.utils import DbCredentials, get_logger
-from bra_interface.maps import Mapper
 
 from dotenv import load_dotenv
 from flask import Flask, render_template, request
+
+from bra_interface.connection import BraDatabase
+from bra_interface.maps import Mapper
+from bra_interface.utils import DbCredentials, get_logger
 
 app = Flask(__name__)
 
@@ -41,6 +42,7 @@ with BraDatabase(credentials=credentials, logger=logger) as database:
 # Create the object allowing to design the map
 mapper = Mapper(logger=logger)
 
+
 def get_specific_bra(massif: str) -> Dict[str, str]:
     """Select the latest BRA of a specific massif"""
     with BraDatabase(credentials=credentials, logger=logger) as database:
@@ -60,27 +62,75 @@ def get_specific_bra(massif: str) -> Dict[str, str]:
         data = database.exec_query(query)[0]
         return data
 
+
+def get_specific_history(massif: str) -> Dict[str, str]:
+    """Select the BRA history of a specific massif"""
+    with BraDatabase(credentials=credentials, logger=logger) as database:
+        query = f"""
+            SELECT date, original_link
+            FROM bra.france
+            WHERE massif = '{massif}';
+            ORDER BY date DESC;
+        """
+        history = [(d["date"].strftime(date_format), d["original_link"]) for d in database.exec_query(query)]
+    return history
+
+
+def get_tab_panel_activity(active: bool = True) -> str:
+    """The first active/inactive refers to the selected tab on top-page.
+    The other one (tab-pane fade) refers to the panel that is indeed printed below.
+    """
+    if active:
+        return "active", "tab-pane fade in active"
+    else:
+        return "inactive", "tab-pane fade"
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # The selected massif for BRA table
+
+    # The selected massif for BRA table. Other variables are used to select the panel/tab once
+    # the page is refreshed.
     try:
-        selected_massif = request.form["selected_massif"]
+        bra_selected_massif = request.form["bra_selected_massif"]
+        activity_bra_tab, activity_bra_div = get_tab_panel_activity(active=True)
     except KeyError:
-        selected_massif = "aravis"
+        bra_selected_massif = "aravis"
+        activity_bra_tab, activity_bra_div = get_tab_panel_activity(active=False)
+
+    # The selected massif for the historical data
+    try:
+        historical_selected_massif = request.form["history_selected_massif"]
+        activity_history_tab, activity_history_div = get_tab_panel_activity(active=True)
+    except KeyError:
+        historical_selected_massif = "aravis"
+        activity_history_tab, activity_history_div = get_tab_panel_activity(active=False)
 
     # Get the BRA of the selected massif
-    selected_bra = get_specific_bra(selected_massif)
+    selected_bra = get_specific_bra(bra_selected_massif)
+    # Get the history of the selected massif
+    selected_history = get_specific_history(historical_selected_massif)
 
     # Create the response context and post as a Jinja template
+    original_link_formated = ".".join(selected_bra['original_link'].split(".")[3:5])
+
+    print(f"BRA: {activity_bra_tab}")
+    print(f"HIS: {activity_history_tab}")
+
     context = {
+        "activity_history_tab": activity_history_tab,
+        "activity_bra_tab": activity_bra_tab,
+        "activity_history_div": activity_history_div,
+        "activity_bra_div": activity_bra_div,
         "contact_email": "emeric.dynomant@gmail.com",
         "home": "https://data-baguette.com",
         "carte_risque_massifs": mapper.get_risk_map(html=True),
         "massifs": available_massifs,
-        "BRA_selected_massif": selected_massif,
+        "BRA_selected_massif": bra_selected_massif,
+        "HISTORY_selected_massif": historical_selected_massif,
         "BRA_data": {
-            "Massif": selected_bra["massif"],
-            "BRA original": f"<a href='{selected_bra['original_link']}'target='_blank'>{selected_bra['original_link']}</a>",
+            "Massif": selected_bra["massif"].title(),
+            "BRA original": f"<a href='{selected_bra['original_link']}'target='_blank'>{original_link_formated}</a>",
             "Date" : selected_bra["date"].strftime(date_format),
             "Jusqu'au" : selected_bra["until"].strftime(date_format),
             "Départs spontanés" : selected_bra["departs"],
@@ -91,7 +141,8 @@ def index():
             "Départ spontanés": selected_bra["departs_spontanes"],
             "Départs provoqués": selected_bra["declanchements_provoques"],
             "Qualité de la neige": selected_bra["qualite_neige"],
-        }
+        },
+        "BRA_history": selected_history
     }
     html_template = render_template("index.html", **context)
     return html_template
